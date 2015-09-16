e980b7cb258ec498d4726a29e5e58eae5fadc4ec
stamepicmorg <stam@epicm.org>
2015-09-16 17:40:21 +0300

    Signed-off-by: stamepicmorg <stam@epicm.org>

--- Sources/BinaryBeer/FrmGame.Designer.cs
+++ Sources/BinaryBeer/FrmGame.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGame));
+            this.SuspendLayout();
+            // 
+            // FrmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmName";
+            this.ClientSize = new System.Drawing.Size(608, 422);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BinaryBeer :: Игра";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
